aece4bf4c0908c77c0529f132c21c6cee8675d36
Jeevan Sandhu <jsand021@wch136-29.cs.ucr.edu>
2019-10-04 12:02:53 -0700

    Completed part 1

---
+++ turnin/jsand021_lab2_part1.c
@@ -0,0 +1,28 @@
+/*	Author: jsand021
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0xFF; PORTB = 0x00;
+
+    while (1) {
+       if((PINA & 0x01) & !(PINA & 0x02)){
+          PORTB = PINB | 0x01;
+       }
+       else{
+          PORTB = 0xFE & PINB;
+       }
+    }
+    return 1;
+}
