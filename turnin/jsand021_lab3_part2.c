d055488d4f4ab37ed4ebc25597d97ef811c0c43f
Jeevan Sandhu <jeevansandhu95@gmail.com>
2019-10-09 19:49:56 -0700

    Completed Lab 3 Exercises 1-5 with proper headers

--- turnin/jsand021_lab3_part2.c
+++ turnin/jsand021_lab3_part2.c
@@ -1,7 +1,7 @@
-/*	Author: jsand021
- *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
+/*	Author: jsand021 - Jeevan Sandhu
+ *  Partner(s) Name: No Partner
+ *	Lab Section: 23
+ *	Assignment: Lab 3  Exercise 2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
